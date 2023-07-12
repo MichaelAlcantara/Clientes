99e22540f308779ea174f10e6e6c386622899c25
MichaelAlcantara <michael.raposo@hotmail.com>
2023-07-12 20:56:52 -0300

    API - Deletar cliente

--- clientes/src/main/java/io/github/michaelAlcantara/rest/ClienteController.java
+++ clientes/src/main/java/io/github/michaelAlcantara/rest/ClienteController.java
@@ -23,6 +23,19 @@ public class ClienteController {
 
     @GetMapping("{id}")
     public Cliente buscarPorId(@PathVariable Integer id){
-        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return repository.
+                findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id){
+        repository.findById(id)
+                .map(cliente -> {
+                    repository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
