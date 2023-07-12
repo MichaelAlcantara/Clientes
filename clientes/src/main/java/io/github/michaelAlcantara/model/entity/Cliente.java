7694182d36a491222e29e3086a584fd0358a1b5d
MichaelAlcantara <michael.raposo@hotmail.com>
2023-07-12 19:33:12 -0300

    criação da entidade de Serviços

--- clientes/src/main/java/io/github/michaelAlcantara/model/entity/Cliente.java
+++ clientes/src/main/java/io/github/michaelAlcantara/model/entity/Cliente.java
@@ -1,8 +1,6 @@
 package io.github.michaelAlcantara.model.entity;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -20,7 +18,7 @@ public class Cliente {
     @Column(nullable = false, length = 11)
     private String cfp;
 
-    @Column
+    @Column(name = "data_cadastro")
     private LocalDate dataCriação;
 
 }
---
+++ clientes/src/main/java/io/github/michaelAlcantara/model/entity/Servico.java
@@ -0,0 +1,27 @@
+package io.github.michaelAlcantara.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Servico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String descricao;
+
+    //Muitos para um
+    //Muitos serviços para um cliente
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+    @Column
+    private BigDecimal valor;
+}
