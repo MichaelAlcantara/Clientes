6711272dfd9b8bef9b9d1f97961896ddbe8df297
MichaelAlcantara <michael.raposo@hotmail.com>
2023-07-29 15:07:15 -0300

    inclusão de dependências do Spring security e JWT

---
+++ clientes/src/main/java/io/github/michaelAlcantara/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package io.github.michaelAlcantara.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableResourceServer
+@EnableAuthorizationServer
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
