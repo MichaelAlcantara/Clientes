113325dd85ccbb6f52fe859d51243a0aa64b2380
MichaelAlcantara <michael.raposo@hotmail.com>
2023-07-28 21:41:06 -0300

    configurando rotas filhas - home

--- cliente-app/src/app/app-routing.module.ts
+++ cliente-app/src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ import { LayoutComponent } from './layout/layout.component';
 const routes: Routes = [
   { path : 'login', component : LoginComponent },
   { path : '', component: LayoutComponent, children:[
-    { path : 'home', component : HomeComponent }
+    { path : 'home', component : HomeComponent },
+    { path : '', redirectTo : '/home', pathMatch: 'full'}
   ]}
 ];
 
